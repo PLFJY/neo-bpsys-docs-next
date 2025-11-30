7aa6dbe54ec4e41930e0cc29965a3e708bf85ef0
ZackZhao233 <wszwbzackzs666@126.com>
2025-11-30 15:41:13 +0800

    新建了文件夹

---
+++ src/.vuepress/navbar/en.ts
@@ -0,0 +1,13 @@
+import { navbar } from "vuepress-theme-hope";
+
+export const enNavbar = navbar([
+  "/",
+  "/portfolio",
+  "/demo/",
+  "/guide/",
+  {
+    text: "V2 Docs",
+    // icon: "book",
+    link: "https://theme-hope.vuejs.press/",
+  },
+]);
